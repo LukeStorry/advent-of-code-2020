1f5994caac13dfba1d94def126aee9d7fda95452
Luke Storry <luke.storry@ghyston.com>
2020-12-01 14:51:15 +0000

    first part of day 1 done

--- rust/src/day1.rs
+++ rust/src/day1.rs
@@ -1,7 +1,27 @@
 use crate::utils;
 
+fn find_corresponding(entries: &Vec<i32>) -> i32 {
+  for entry in entries {
+    let corresponding_entry = 2020 - entry;
+    if entries.contains(&corresponding_entry) {
+      return entry * corresponding_entry;
+    }
+  }
+  0
+}
+
 pub fn part1() {
   let input = utils::get_input(1);
+  print!("{}", find_corresponding(&input))
+}
 
-  print!("{:?}", input);
+#[cfg(test)]
+mod tests {
+  use super::*;
+
+  #[test]
+  fn example_1() {
+    let data = vec![1721, 979, 366, 299, 675, 1456];
+    assert_eq!(find_corresponding(&data), 514579)
+  }
 }
--- rust/src/utils.rs
+++ rust/src/utils.rs
@@ -3,8 +3,9 @@ use std::fs::read_to_string;
 pub fn get_input(day: u8) -> Vec<i32> {
     read_to_string(format!("../inputs/{}.txt", day))
         .unwrap()
-        .trim()
-        .split('\n')
-        .map(|int| int.trim().parse().unwrap())
+        .lines()
+        .map(|l| l.trim())
+        .filter(|l| !l.is_empty())
+        .map(|l| l.parse().unwrap())
         .collect()
 }

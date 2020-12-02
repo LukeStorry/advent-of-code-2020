085c7c95b6266f73d3d86a8bb7c459a1bbb95102
Luke Storry <luke.storry@ghyston.com>
2020-12-02 12:45:11 +0000

    moved to using filters for day2

--- python/day2.py
+++ python/day2.py
@@ -1,5 +1,11 @@
 import re
 
+with open("inputs/2.txt") as file:
+    data = file.read()
+# data = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
+
+rules = re.findall(r"(\d+)-(\d+) (.):(.*)", data)
+
 def valid1(rule):
     count = rule[3].count(rule[2])
     return count >= int(rule[0]) and count <= int(rule[1])
@@ -9,16 +15,9 @@ def valid2(rule):
     char1 = password[int(rule[0])]
     char2 = password[int(rule[1])]
     expect = rule[2]
-    return (char1==expect or char2==expect) and char1!=char2
+    return (char1==expect) != (char2==expect)
 
-
-with open("inputs/2.txt") as file:
-    data = file.read()
-# data = "1-3 a: abcde\n1-3 b: cdefg\n2-9 c: ccccccccc"
-
-rules = re.findall(r"(\d+)-(\d+) (.):(.*)", data)
-
-part1 = sum(1 for x in rules if valid1(x))
+part1 = len(list(filter(valid1,rules)))
 print(f"Day 2 part 1: {part1}")
-part2 = sum(1 for x in rules if valid2(x))
+part2 = len(list(filter(valid2,rules)))
 print(f"Day 2 part 2: {part2}")

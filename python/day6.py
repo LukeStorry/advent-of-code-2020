07a946ee29c52f0ede3708499176c7920b841aa1
Luke Storry <luke.storry@ghyston.com>
2020-12-07 09:16:25 +0000

    day 6 part 2 done

--- python/day6.py
+++ python/day6.py
@@ -1,9 +1,14 @@
-from typing import Set
-
 with open("../inputs/6.txt") as file:
     data = file.read().strip()
 # data = "abc\n\na\nb\nc\n\nab\nac\n\na\na\na\na\n\nb"
 
-count = [len(set(answer for person in group.splitlines() for answer in person)) for group in (data.split("\n\n"))]
+groups = data.split("\n\n")
 
-print(sum(count)) # 6170
+part1 = sum(len(set([answer for person in group.splitlines() for answer in person])) for group in groups)
+print(part1)
+
+answers_sets = [[set(answer) for answer in group.splitlines()] for group in groups]
+all_answered = 0
+for s in answers_sets:
+    all_answered += len(s[0].intersection(*s[1:]))
+print(all_answered)

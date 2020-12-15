359135006063632e4c9f2e6656064af69f576385
Luke Storry <luke.storry@ghyston.com>
2020-12-15 08:29:57 +0000

    day 15 part 2

--- python/day15.py
+++ python/day15.py
@@ -3,11 +3,13 @@ numbers = [int(n) for n in data.split(',')]
 
 last_spoken = {n: i for i, n in enumerate(numbers, start=1)}
 last_number = 0
-for turn in range(len(numbers) + 2, 2021):
+for turn in range(len(numbers) + 1, 30000000):
     if last_number in last_spoken:
-        next_number = turn - 1 - last_spoken[last_number]
+        next_number = turn - last_spoken[last_number]
     else:
         next_number = 0
-    last_spoken[last_number] = turn - 1
+    last_spoken[last_number] = turn
     last_number = next_number
-print(last_number)
+    if turn == 2019:
+        print(f"Part 1: {last_number}")
+print(f"Part 2: {last_number}")

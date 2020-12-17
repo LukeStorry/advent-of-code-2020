0dca87d00786310abdeb4e0d94d054dd9fc5e565
Luke Storry <luke.storry@ghyston.com>
2020-12-17 10:11:23 +0000

    part 2

--- python/day17.py
+++ python/day17.py
@@ -1,10 +1,10 @@
 with open("../inputs/17.txt") as file:
     data = file.read()
+
 # data = ".#.\n..#\n###"
 
-actives = set((x, y, 0) for (y, line) in enumerate(data.splitlines()) for (x, char) in enumerate(line) if char == '#')
 
-def neighbours(x, y, z):
+def neighbours_3d(x, y, z):
     return [(_x, _y, _z)
             for _x in (x - 1, x, x + 1)
             for _y in (y - 1, y, y + 1)
@@ -12,27 +12,32 @@ def neighbours(x, y, z):
             if (_x, _y, _z) != (x, y, z)]
 
 
-def print_pocket(coords):
-    min_x, max_x = min(x for (x, y, z) in coords) - 1, max(x for (x, y, z) in coords) + 2
-    min_y, max_y = min(y for (x, y, z) in coords) - 1, max(y for (x, y, z) in coords) + 2
-    min_z, max_z = min(z for (x, y, z) in coords), max(z for (x, y, z) in coords) + 1
-    for z in range(min_z, max_z):
-        print(f"z={z}")
-        for y in range(min_y, max_y):
-            print(''.join('#' if (x, y, z) in coords else '.' for x in range(min_x, max_x)))
+def neighbours_4d(w, x, y, z):
+    return [(_w, _x, _y, _z)
+            for _w in (w - 1, w, w + 1)
+            for _x in (x - 1, x, x + 1)
+            for _y in (y - 1, y, y + 1)
+            for _z in (z - 1, z, z + 1)
+            if (_w, _x, _y, _z) != (w, x, y, z)]
 
 
-for cycle in range(6):
-    # print(f"After {cycle} cycles:")
-    # print_pocket(actives)
-    new_actives = set()
-    all_coords = set(list(actives) + [n for active in actives for n in neighbours(*active)])
-    for cube in all_coords:
-        active_neighbours = sum(neighbour in actives for neighbour in neighbours(*cube))
-        if cube in actives and active_neighbours in [2, 3]:
-            new_actives.add(cube)
-        if cube not in actives and active_neighbours == 3:
-            new_actives.add(cube)
-    actives = new_actives
+def solve(actives, neighbours_fn):
+    for cycle in range(6):
+        new_actives = set()
+        all_coords = set(list(actives) + [n for active in actives for n in neighbours_fn(*active)])
+        for cube in all_coords:
+            active_neighbours = sum(neighbour in actives for neighbour in neighbours_fn(*cube))
+            if cube in actives and active_neighbours in [2, 3]:
+                new_actives.add(cube)
+            if cube not in actives and active_neighbours == 3:
+                new_actives.add(cube)
+        actives = new_actives
+    return len(actives)
 
-print(f"Part 1: {len(actives)}")
+
+initial_1 = set((x, y, 0) for (y, line) in enumerate(data.splitlines()) for (x, char) in enumerate(line) if char == '#')
+print(f"Part 1: {solve(initial_1, neighbours_3d)}")
+
+initial_2 = set(
+    (x, y, 0, 0) for (y, line) in enumerate(data.splitlines()) for (x, char) in enumerate(line) if char == '#')
+print(f"Part 2: {solve(initial_2, neighbours_4d)}")

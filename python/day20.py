b0a9192e49c390a320b968896d449e140aa60dfd
Luke Storry <luke.storry@ghyston.com>
2020-12-20 21:17:25 +0000

    day 20 part 1

---
+++ python/day20.py
@@ -0,0 +1,21 @@
+with open("../inputs/20.txt") as file:
+    data = file.read().strip()
+from math import prod
+
+# data = "Tile 2311:\n..##.#..#.\n##..#.....\n#...##..#.\n####.#...#\n##.##.###.\n##...#.###\n.#.#.#..##\n..#....#..\n###...#.#.\n..###..###\n\nTile 1951:\n#.##...##.\n#.####...#\n.....#..##\n#...######\n.##.#....#\n.###.#####\n###.##.##.\n.###....#.\n..#.#..#.#\n#...##.#..\n\nTile 1171:\n####...##.\n#..##.#..#\n##.#..#.#.\n.###.####.\n..###.####\n.##....##.\n.#...####.\n#.##.####.\n####..#...\n.....##...\n\nTile 1427:\n###.##.#..\n.#..#.##..\n.#.##.#..#\n#.#.#.##.#\n....#...##\n...##..##.\n...#.#####\n.#.####.#.\n..#..###.#\n..##.#..#.\n\nTile 1489:\n##.#.#....\n..##...#..\n.##..##...\n..#...#...\n#####...#.\n#..#.#.#.#\n...#.#.#..\n##.#...##.\n..##.##.##\n###.##.#..\n\nTile 2473:\n#....####.\n#..#.##...\n#.##..#...\n######.#.#\n.#...#.#.#\n.#########\n.###.#..#.\n########.#\n##...##.#.\n..###.#.#.\n\nTile 2971:\n..#.#....#\n#...###...\n#.#.###...\n##.##..#..\n.#####..##\n.#..####.#\n#..#.#..#.\n..####.###\n..#.#.###.\n...#.#.#.#\n\nTile 2729:\n...#.#.#.#\n####.#....\n..#.#.....\n....#..#.#\n.##..##.#.\n.#.####...\n####.#.#..\n##.####...\n##..#.##..\n#.##...##.\n\nTile 3079:\n#.#.#####.\n.#..######\n..#.......\n######....\n####.#..#.\n.#...#.##.\n#.#####.##\n..#.###...\n..#.......\n..#.###..."
+
+tiles = {int(title[4:-1]): image for title, *image in (tile.splitlines() for tile in data.split('\n\n'))}
+
+
+def adjacent(tile1, tile2):
+    def borders_list(tile):
+        return [tile[0], tile[-1], ''.join(row[-1] for row in tile), ''.join(row[0] for row in tile)]
+
+    return any(
+        border1 == border2 or border1 == border2[::-1]
+        for border1 in borders_list(tile1) for border2 in borders_list(tile2)
+    )
+
+
+corners = [tile for tile, image in tiles.items() if 3 == sum(adjacent(image, other) for other in tiles.values())]
+print(prod(corners))
